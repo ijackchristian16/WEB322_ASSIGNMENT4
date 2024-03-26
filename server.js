bd6c9c2e482a655bc0775f0424fe90ba4c048dd8
stallioninfotech238 <134456597+ijackchristian16@users.noreply.github.com>
2024-03-26 10:01:37 +0530

    init

---
+++ server.js
@@ -0,0 +1,66 @@
+/********************************************************************************
+*  WEB322 – Assignment 04
+* 
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Isaac Christian Student ID: 137671202 Date: 26/03/2024
+*
+*  Published URL: 
+*
+********************************************************************************/
+const legoData = require("./modules/legoSets");
+const path = require("path");
+
+const express = require('express');
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render("home")
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get("/lego/sets", async (req,res)=>{
+
+  let sets = [];
+
+  try{    
+    if(req.query.theme){
+      sets = await legoData.getSetsByTheme(req.query.theme);
+    }else{
+      sets = await legoData.getAllSets();
+    }
+
+    res.render("sets", {sets})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+  
+});
+
+app.get("/lego/sets/:num", async (req,res)=>{
+  try{
+    let set = await legoData.getSetByNum(req.params.num);
+    res.render("set", {set})
+  }catch(err){
+    res.status(404).render("404", {message: err});
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+});
+
+legoData.initialize().then(()=>{
+  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+});
